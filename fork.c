b832c76c2bf0af66ee7cc8655b4bfa875b6b50b0
Ricky Chen <rchen10@stuy.edu>
2017-11-17 00:24:46 +0000

    Making working program. Needs clean-up.

--- fork.c
+++ fork.c
@@ -7,21 +7,25 @@
 
 int main(){
   printf("Parent here, waddup my dudes.\n");
-  int sleep_time = 0;
-  int *xd = &sleep_time;
+  
+  srand(time(NULL));
+  int sleep_time = (rand() % 16) + 5;
 
+  //sleep(10); re-seed works if given enough time
+
+  if (fork()){ //randomize first parent
+    srand(time(NULL));
+    sleep_time = (rand() % 16) + 5;
+  }
   int fs = fork();
-  //fork();
+  
   int *status;
 
   if (fs){
     int cpid = wait(status);
-    printf("%d slept for %p seconds\n", cpid, &sleep_time);
+    printf("%d slept for %d seconds\n", cpid, sleep_time);
   }
   else{
-    srand(time(NULL));
-    sleep_time = rand() % 16 + 5;
-    printf("%p\n", &sleep_time);
     sleep(sleep_time);
     printf("lol xdd\n");
   }
